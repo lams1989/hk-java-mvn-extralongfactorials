40fdf073aefa6178477eb7f5281b9dafe2b7a6de
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-07-23 18:38:18 -0300

    test implementation, pom modified

--- src/test/java/com/lams1989/extralongfactorials/ExtraLongFactorialsTest.java
+++ src/test/java/com/lams1989/extralongfactorials/ExtraLongFactorialsTest.java
@@ -8,10 +8,10 @@ public class ExtraLongFactorialsTest extends TestCase{
 
 		public  void test1() {
 			BigInteger factorial = BigInteger.valueOf(1L);
-
+			BigInteger expected = new BigInteger("93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000");
 			int n = 100;
 			factorial = ExtraLongFactorials.extraLongFactorials(n);
-			assertEquals(true, factorial);
+			assertTrue(expected.equals(factorial));
 		}
 		public  void test2() {
 			int n = 500;
